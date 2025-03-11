4b441fc59a7ba9ed66a8abe06ffb7e8244f2c641
js-harang <harang3427@naver.com>
2025-03-11 18:14:30 +0900

    _

--- TicTacToe/Assets/Scripts/Common/Constants.cs
+++ TicTacToe/Assets/Scripts/Common/Constants.cs
@@ -5,9 +5,13 @@ public class Constants
 
     public enum MultiplayManagerState
     {
-        CreateRoom,
-        JoinRoom,
-        StartGame,
-        EndGame
+        CreateRoom,     // 방 생성
+        JoinRoom,       // 생성된 방에 참여
+        StartGame,      // 생성한 방에 다른 유저가 참여해서 게임 시작
+        ExitRoom,       // 자신이 방을 빠져 나왔을 때
+        EndGame         // 상대방이 접속을 끊거나 방을 나갔을 때
     };
+    
+    public enum PlayerType { None, PlayerA, PlayerB }
+    public enum GameType { SinglePlayer, DualPlayer, MultiPlayer }
 }
